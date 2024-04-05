3a789c18d0c303e4c5a5a2877bcd6c3a21bedfbc
Vehbi Taha Edis <93030513+C17H19NO3-SS@users.noreply.github.com>
2024-04-06 00:13:17 +0300

    New constants added.

--- consts.js
+++ consts.js
@@ -1,21 +1,34 @@
 import crypto from "crypto";
 
-const imp = import.meta.url.slice("file://".length, -"consts.js".length);
+export const PROJECT_DIRECTORY = import.meta.url.slice(
+	"file://".length,
+	-"consts.js".length,
+);
 
 export const DEFAULT_PORT = 1001;
 
 export const DEFAULT_HOST = "0.0.0.0";
 
-export const DEFAULT_LOG_FILE = `${imp}log.txt`;
+export const DEFAULT_LOG_FILE = `${PROJECT_DIRECTORY}log.txt`;
+
+export const DEFAULT_ERROR_LOG_FILE = "error.log.txt";
 
 export const DEFAULT_LOG_FORMAT = "D:P:M:B:Q:I"; // Date:Path,Method,Post,Query
 
-export const DEFAULT_LOG_FILE_BACKUP_DIR = `${imp}Logs`;
+export const DEFAULT_LOG_FILE_BACKUP_DIR = `${PROJECT_DIRECTORY}Logs`;
 
-export const DEFAULT_LOOPS_DIR = `${imp}Loops`;
+export const DEFAULT_LOOPS_DIR = `${PROJECT_DIRECTORY}Loops`;
 
-export const DEFAULT_FILEMANAGER_DIR = `${imp}files`;
+export const DEFAULT_FILEMANAGER_DIR = `${PROJECT_DIRECTORY}files`;
+
+export const DEFAULT_OVERWRITE = true;
 
 export const DEFAULT_PASSWORD = "{login:'success',}";
+
+export const DEFAULT_PROCESS_USER = 1000;
+
+export const DEFAULT_PROCESS_LOG_DELETE_TIMEOUT = 1000 * 60 * 60 * 24 * 3; // 3 day
+
+export const DEFAULT_PROCESS_SCRIPT_DELETE_TIMEOUT = 1000 * 60 * 60; // 1 hour
 
 export const TOKEN = crypto.randomBytes(64).toString("hex");
