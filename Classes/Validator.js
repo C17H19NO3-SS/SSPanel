5f117d5d957aad4e6349d1f96c6f0918f74f7385
Vehbi Taha Edis <93030513+C17H19NO3-SS@users.noreply.github.com>
2024-04-20 16:59:31 +0300

    Validator.js update

--- Classes/Validator.js
+++ Classes/Validator.js
@@ -5,10 +5,12 @@ import { DEFAULT_FILEMANAGER_DIR, DEFAULT_FILE_PATH_VALIDATE_COMMAND } from '../
 
 export default class Validator {
     ValidateFilePath(path) {
-        if(fs.existsSync(path)) {
-            if(cp.execSync(`${DEFAULT_FILE_PATH_VALIDATE_COMMAND} ${path}`).toString().startsWith(DEFAULT_FILEMANAGER_DIR)) {
-                return true;
-            } else return false;
-        } else return false;
+        var stat = {};
+        if(fs.existsSync(path))
+            if(cp.execSync(`${DEFAULT_FILE_PATH_VALIDATE_COMMAND} ${path}`).toString().startsWith(DEFAULT_FILEMANAGER_DIR))
+                stat = {exist:true, success:true}; 
+            else stat = {exist: true, success: false};
+        else stat = {exist: false, success: false};
+        return stat;
     }
 }
