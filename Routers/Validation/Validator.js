62e16768851f2d2b80df259c1972a587ff0b53b5
Vehbi Taha Edis <93030513+C17H19NO3-SS@users.noreply.github.com>
2024-04-20 17:05:24 +0300

    Validator.js UPDATE

--- Routers/Validation/Validator.js
+++ Routers/Validation/Validator.js
@@ -1,19 +1,26 @@
 import { Router } from 'express';
-import { DEFAULT_FILE_PATH_PARAMETER_NAMES } from '../../consts.js';
+import { DEFAULT_FILE_PATH_PARAMETER_NAMES, DEFAULT_FILEMANAGER_DIR } from '../../consts.js';
 import Validator from '../../Classes/Validator.js';
 import ErrorResponser from '../../Classes/Error.js';
+import Utils from '../../Utils/Utils.js';
 
 export default Router()
     .use((req, res, next) => {
         var response;
         const validator = new Validator();
+        req.paramStat = {
+            query: {},
+            body: {}
+        };
         for(const param of DEFAULT_FILE_PATH_PARAMETER_NAMES) {
             if(response !== undefined) continue;
-            if(req.query[param] || req.body[param]) {
-                response = validator.ValidateFilePath(req.query[param] || req.body[param]) === false ? new ErrorResponser(res) : undefined;
+            if(req.query[param]) {
+                req.paramStat.query[param] = validator.ValidateFilePath(`${DEFAULT_FILEMANAGER_DIR}/${req.query[param]}`);
+            }
+            if(req.body[param]) {
+                req.paramStat.body[param] = validator.ValidateFilePath(`${DEFAULT_FILEMANAGER_DIR}/${req.body[param]}`);
             }
         }
-        if(response !== undefined)
-            response?.send(req.i18n.t('validation.error'));
-        else next();
+        req.Utils = Utils;
+        next();
     });
